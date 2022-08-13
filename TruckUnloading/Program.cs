db2ec1001c411abf38a981b6437de9fc47b08e45
CurtisLV <curtislv@gmail.com>
2022-08-13 15:10:18 +0300

    Truck loader

---
+++ TruckUnloading/Program.cs
@@ -0,0 +1,12 @@
+﻿using TruckUnloading;
+
+Truck truck = new Truck();
+
+truck.Load(1000);
+truck.PrintLoad();
+truck.Load(500);
+truck.PrintLoad();
+truck.Unload(300);
+truck.PrintLoad();
+truck.Unload(1000);
+truck.PrintLoad();
---
+++ TruckUnloading/Truck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckUnloading;
+
+internal class Truck
+{
+    int totalLoad = 0;
+    int maxLoad = 3000;
+
+    public void Load(int weight)
+    {
+        if (totalLoad + weight <= maxLoad)
+        {
+            totalLoad += weight;
+        }
+    }
+    public void Unload(int weight)
+    {
+        if (totalLoad - weight >= 0)
+        {
+            totalLoad -= weight;
+        }
+    }
+
+    public void PrintLoad()
+    {
+        Console.WriteLine($"Currently there is {totalLoad} kg loaded in the truck");
+    }
+
+
+
+}
