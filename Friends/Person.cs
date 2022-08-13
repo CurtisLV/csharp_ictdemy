1cb905ca4692067252e1e3339f1ca95588495902
CurtisLV <curtislv@gmail.com>
2022-08-13 15:17:16 +0300

    Friend greetings

---
+++ Friends/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+        public Person friend;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {name}, I'm {age} years old and my friend is {friend}");
+        }
+    }
+}
