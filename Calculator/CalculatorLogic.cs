483615f56e09baf9efdc478d2cc2e204a34af2ed
CurtisLV <curtislv@gmail.com>
2022-08-13 13:32:25 +0300

    Basic calculator OOP

---
+++ Calculator/CalculatorLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class CalculatorLogic
+    {
+        double num1 = 0;
+        double num2 = 0;
+        public CalculatorLogic(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public double Sum()
+        {
+            return num1 + num2;
+        }
+
+        public double Difference()
+        {
+            return num1 - num2;
+        }
+
+        public double Product()
+        {
+            return (num1 * num2);  
+        }
+
+        public double Quotient()
+        {
+            return num1 / num2; 
+        }
+
+        //public double Num1
+        //{
+        //    get; set; 
+        //}
+        //public double Num2
+        //{
+        //    get; set;
+        //}
+
+
+
+
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Calculator;
+
+Console.WriteLine("Enter 1st number: ");
+double num1 = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter 2nd number: ");
+double num2 = Double.Parse(Console.ReadLine());
+
+CalculatorLogic calc = new CalculatorLogic(num1, num2);
+
+Console.WriteLine($"Sum: {calc.Sum()}");
+Console.WriteLine($"Difference: {calc.Difference()}");
+Console.WriteLine($"Product: {calc.Product()}");
+Console.WriteLine($"Quotient: {calc.Quotient()}");
