3a1147d9265a54ab8fcd096113e4147b113fc720
CurtisLV <curtislv@gmail.com>
2022-08-15 21:27:57 +0300

    21:27

--- Arena/Program.cs
+++ Arena/Program.cs
@@ -3,8 +3,18 @@
 RollingDie dieSix = new RollingDie();
 RollingDie dieTen = new RollingDie(10);
 
-Console.WriteLine(dieSix.GetSidesCount());
-Console.WriteLine(dieTen.GetSidesCount());
-
+// Rolls the 6-sided die
 Console.WriteLine(dieSix);
+for (int i = 0; i < 10; i++) 
+{
+    Console.Write(dieSix.Roll() + " ");
+}
+    
 
+// Rolls the 10-sided die
+Console.WriteLine("\n\n" + dieTen);
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(dieTen.Roll() + " ");
+}
+    
