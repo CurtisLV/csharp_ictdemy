6680c9ee29ddc6ae0dd937a09a1f830abe3f80ec
CurtisLV <curtislv@gmail.com>
2022-08-15 21:05:57 +0300

    Creating board game Arena

---
+++ Arena/RollingDie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena;
+/// <summary>
+/// Class representing a die for a board game
+/// </summary>
+internal class RollingDie
+{
+    private Random random;
+    private int sidesCount;
+}
