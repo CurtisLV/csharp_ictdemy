d706ad298419d7472d9903ba720481929fcfa45b
CurtisLV <curtislv@gmail.com>
2022-08-17 19:58:03 +0300

    Sentence generator

---
+++ SentenceGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using SentenceGenerator;
+
+SentenceGen words = new SentenceGen();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(words.Generate());
+}
+
+
+
---
+++ SentenceGenerator/SentenceGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceGenerator;
+
+internal class SentenceGen
+{
+    private string[] attributes = { "blue", "big", "thin", "best", "automatic" };
+    private string[] subjects = { "unicorn", "programmer", "manager", "hippopotamus", "T-rex" };
+    private string[] adverbs = { "goes to", "likes to", "loves to", "learns to", "wants to" };
+    private string[] verbs = { "sleep", "lay", "cook", "clean", "play" };
+    private string[] advPlace = {"on the table", "in the forest", "under the desk", "at grandma's", "at work"};
+
+    Random rand = new Random();
+
+    private string RandWord(string[] array)
+    {
+        int index = rand.Next(array.Length);
+        string word = array[index];
+        return word;
+    }
+
+    public string Generate()
+    {
+        return $"{RandWord(attributes)} {RandWord(subjects)} {RandWord(adverbs)} {RandWord(verbs)} {RandWord(advPlace)}";
+    }
+
+
+}
