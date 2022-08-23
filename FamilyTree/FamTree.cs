2ef39e343fa7e1ab33a7c0121204357c68db8fab
CurtisLV <curtislv@gmail.com>
2022-08-23 15:08:46 +0300

    Family Tree console app

---
+++ FamilyTree/FamTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree;
+
+internal class FamTree
+{
+
+    private Person root;
+
+    public FamTree(Person root)
+    {
+        this.root = root;
+    }
+}
---
+++ FamilyTree/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree;
+
+internal class Person
+{
+    private string name;
+    private Person mother;
+    private Person father;
+
+    public Person(string name, Person mother, Person father)
+    {
+        this.name = name;
+        this.mother = mother;
+        this.father = father;
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+}
