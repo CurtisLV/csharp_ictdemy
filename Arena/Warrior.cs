9c7815caefb5299265cd8e1f56f3dcc01a1cf2af
CurtisLV <curtislv@gmail.com>
2022-08-23 17:04:41 +0300

    17:04

--- Arena/Warrior.cs
+++ Arena/Warrior.cs
@@ -33,5 +33,38 @@ internal class Warrior
     /// </summary>
     private RollingDie die;
 
+    public Warrior(string name, int health, int damage, int defense, RollingDie die)
+    {
+        this.name = name;
+        this.health = health;
+        this.maxHealth = health;
+        this.damage = damage;
+        this.defense = defense;
+        this.die = die;
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public bool Alive()
+    {
+        return (health > 0);
+    }
+
+    public string HealthBar()
+    {
+        string s = "[";
+        int total = 20;
+        double count = Math.Roung(((double)health / maxHealth) * total);
+        if ((count == 0) && Alive()) // because everything below 2.5% health would be rounded to 0
+        {
+            count = 1;
+        }
+    }
+
+
+
 
 }
