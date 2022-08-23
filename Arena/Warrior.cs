2b779226a9b4f8e194f52b29be4940bcb667e249
CurtisLV <curtislv@gmail.com>
2022-08-23 16:54:16 +0300

    Warrior class

---
+++ Arena/Warrior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena;
+
+internal class Warrior
+{
+    /// <summary>
+    /// Warrior's name
+    /// </summary>
+    private string name;
+    /// <summary>
+    /// Health in HP
+    /// </summary>
+    private int health;
+    /// <summary>
+    /// Max health in HP
+    /// </summary>
+    private int maxHealth;
+    /// <summary>
+    ///  Damage in HP
+    /// </summary>
+    private int damage;
+    /// <summary>
+    /// Defense in HP
+    /// </summary>
+    private int defense;
+    /// <summary>
+    /// Rolling dice instance
+    /// </summary>
+    private RollingDie die;
+
+
+}
