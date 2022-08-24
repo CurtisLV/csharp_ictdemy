a0c6a6b5db5eba01ff66e7ad507a9aa2e8ad4445
CurtisLV <curtislv@gmail.com>
2022-08-24 15:18:17 +0300

    main ready for fight

--- Arena/Program.cs
+++ Arena/Program.cs
@@ -1,13 +1,11 @@
 ﻿using Arena;
 
 RollingDie die = new RollingDie(10);
-Warrior warrior = new Warrior("Karlis", 100, 20, 10, die);
 
-Warrior enemy = new Warrior("Shadow", 60, 18, 15, die);
+Warrior karlis = new Warrior("Karlis", 100, 20, 10, die);
+Warrior shadow = new Warrior("Shadow", 60, 18, 15, die);
 
+Arena.Arena arena = new Arena.Arena(karlis, shadow, die);
 
-Console.WriteLine(enemy.GetLastMessage());
-Console.WriteLine(warrior.GetLastMessage());
-
-Console.WriteLine($"Health after the hit {warrior.HealthBar()}");
+arena.Fight();
 
