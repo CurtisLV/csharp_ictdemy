bfbfe3a8d8f035ce8d1b552e815928dc0e240b0f
CurtisLV <curtislv@gmail.com>
2022-08-25 18:49:37 +0300

    Adding Mage class

---
+++ Arena/Mage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena;
+
+internal class Mage: Warrior
+{
+    private int Mana;
+    private int maxMana;
+    private int magicDefence;
+
+
+
+}
