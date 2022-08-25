461999e215b5fd0db7b7fe944b76e2cfa4ca62b6
CurtisLV <curtislv@gmail.com>
2022-08-25 19:22:32 +0300

    Mage Attack() with mana

--- Arena/Mage.cs
+++ Arena/Mage.cs
@@ -10,17 +10,39 @@ internal class Mage: Warrior
 {
     private int mana;
     private int maxMana;
-    private int magicDefence;
+    private int magicDamage;
 
-    public Mage(string name, int health, int damage, int defense, RollingDie die, int mana, int magicDefence): base(name, health, damage, defense, die)
+    public Mage(string name, int health, int damage, int defense, RollingDie die, int mana, int magicDamage): base(name, health, damage, defense, die)
     {
         this.mana = mana;
         this.maxMana = mana;
-        this.magicDefence = magicDefence;
+        this.magicDamage = magicDamage;
     }
 
 
+    public override void Attack(Warrior enemy)
+    {
+        int hit = 0;
 
+        // when Mana isn't full
+        if (mana < maxMana)
+        {
+            mana += 10;
+            if (mana > maxMana)
+            {
+                mana = maxMana;
+            }
+            hit = damage + die.Roll();
+            SetMessage($"{name} attacks with a hit worth {hit} HP");
+        } else // Magic defense
+        {
+            hit = magicDamage + die.Roll();
+            SetMessage($"{name} attacks with a hit worth {hit} HP");
+            mana = 0;
+        }
+        enemy.Defend(hit);
+
+    }
 
 
 }
