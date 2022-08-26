c3d6db6c8d83deccd22360d225b41b8ec7f51d35
CurtisLV <curtislv@gmail.com>
2022-08-26 21:17:32 +0300

    Usability test

--- HumanRun/Program.cs
+++ HumanRun/Program.cs
@@ -6,3 +6,11 @@ charles.Run(10);
 charles.Run(10);
 charles.Sleep(1);
 charles.Run(10);
+
+CSharpProgrammer karlis = new CSharpProgrammer("Karlis", 33, "Visual Studio");
+Console.WriteLine(karlis);
+karlis.Program(200);
+karlis.Run(10);
+karlis.Sleep(1);
+karlis.Program(5000);
+
