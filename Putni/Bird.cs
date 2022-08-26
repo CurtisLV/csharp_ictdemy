537389e448a343a0896d319889c704c970aed302
CurtisLV <curtislv@gmail.com>
2022-08-26 19:22:12 +0300

    Bird class exercise

---
+++ Putni/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Putni;
+
+internal class Bird
+{
+    protected int hunger;
+    protected int weight;
+
+    public Bird(int hunger, int weight)
+    {
+        this.hunger = hunger;
+        this.weight = weight;
+    }
+
+    public void Eat(int foodWeight)
+    {
+        hunger -= foodWeight;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+
+        weight += foodWeight;
+    }
+}
