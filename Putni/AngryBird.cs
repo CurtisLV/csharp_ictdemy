8855104222a487af4306c229bff025f8f7e3089c
CurtisLV <curtislv@gmail.com>
2022-08-26 19:24:40 +0300

    AngryBird class

---
+++ Putni/AngryBird.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Putni;
+
+internal class AngryBird: Bird
+{
+    private int anger;
+
+    public void Provoke(int infuriation)
+    {
+    }
+
+
+}
