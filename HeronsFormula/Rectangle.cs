bf6ef13182f16644e84ce387bc88e6ea79b0ea36
CurtisLV <curtislv@gmail.com>
2022-08-27 14:21:59 +0300

    Adding Triangle class

---
+++ HeronsFormula/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeronsFormula;
+
+internal class Rectangle: Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(string color, double width, double height): base (color)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double Area()
+    {
+        return width * height;
+    }
+}
