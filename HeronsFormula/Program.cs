aa19ef3b65c79614462c3c7107b9179126aae2d8
CurtisLV <curtislv@gmail.com>
2022-08-27 14:39:20 +0300

    Main logic comments

--- HeronsFormula/Program.cs
+++ HeronsFormula/Program.cs
@@ -1,2 +1,9 @@
 ﻿using HeronsFormula;
 
+// calculate area of the tree https://www.ictdemy.com/images/11/csp/exercise/tree.png
+
+    // create array of 5 shapes
+
+    // calculate 4 triangles 15x15x25 cm
+
+    //calculate tree area 3x26 cm
