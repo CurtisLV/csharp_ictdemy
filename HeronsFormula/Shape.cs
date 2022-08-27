44345f04a178526e16ea5dd8cc44494c2490e927
CurtisLV <curtislv@gmail.com>
2022-08-27 14:04:15 +0300

    Heron's formula for shapes

---
+++ HeronsFormula/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeronsFormula;
+
+internal class Shape
+{
+    protected string color;
+
+    public Shape(string color)
+    {
+        this.color = color;
+    }
+}
