9c9e9d385f27690c7214756eb1f9d7daac160e81
CurtisLV <curtislv@gmail.com>
2022-08-29 17:26:18 +0300

    Basic dice logic

--- RollDice2/RollingDice.cs
+++ RollDice2/RollingDice.cs
@@ -9,8 +9,11 @@ namespace RollDice2;
 internal class RollingDice
 {
     private static Random random = new Random();
-    
+
     public int Throw()
+    {
+        return random.Next(6) + 1;
+    }
 
 
 }
