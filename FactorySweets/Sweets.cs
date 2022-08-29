6daf17a4ed018a6c4051102800a48826f3320d8b
CurtisLV <curtislv@gmail.com>
2022-08-29 16:47:42 +0300

    Sweets class logic

--- FactorySweets/Sweets.cs
+++ FactorySweets/Sweets.cs
@@ -8,5 +8,20 @@ namespace FactorySweets
 {
     internal class Sweets
     {
+        private string color;
+        private string shape;
+        private int weight;
+
+        public Sweets(string color, in string shape, int weight)
+        {
+            this.color = color;
+            this.shape = shape;
+            this.weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"A {shape}, {color} candy that weights {weight}g";
+        }
     }
 }
