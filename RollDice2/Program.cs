8ef5f8e077ab640d3b3f3fdc3151d8a4fce0e0c8
CurtisLV <curtislv@gmail.com>
2022-08-29 17:34:12 +0300

    Main logic

--- RollDice2/Program.cs
+++ RollDice2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RollDice2;
+
+RollingDice d1 = new RollingDice();
+RollingDice d2 = new RollingDice();
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine($"The first die rolled a\t{d1.Throw()}");
+    Console.WriteLine($"The second die rolled a\t{d2.Throw()}");
+}
