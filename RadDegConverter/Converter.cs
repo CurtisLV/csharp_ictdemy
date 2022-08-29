049b1284dff6026821332070108d5ffded5a67ba
CurtisLV <curtislv@gmail.com>
2022-08-29 12:22:01 +0300

    Both functions finished

---
+++ RadDegConverter/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadDegConverter;
+
+internal class Converter
+{
+
+
+    static public double RadiansToDegrees(double rad)
+    {
+        // degrees = radians × 180° / π
+        double deg = rad * (180 / Math.PI);
+        return deg;
+    }
+
+
+    static public double DegreesToRadians(double deg)
+    {
+        // 1 rad = 180°/π = 57.295779513°
+        double rad = deg / (180 / Math.PI);
+        return rad;
+    }
+
+
+
+
+
+
+}
