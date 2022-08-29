c7c93e193069f15a521088b8047b405e8fa34f96
CurtisLV <curtislv@gmail.com>
2022-08-29 16:55:16 +0300

    Sweets Factory logic

--- FactorySweets/SweetsFactory.cs
+++ FactorySweets/SweetsFactory.cs
@@ -10,12 +10,19 @@ internal class SweetsFactory
 {
 
 
-    public static Sweets CreateBanana()
+    public static Sweet CreateBanana()
     {
-        return new Sweet()
-        {
+        return new Sweet("yellow", "round", 15);
+    }
 
-        }
+    public static Sweet CreateStrawberry()
+    {
+        return new Sweet("red", "round", 15);
+    }
+
+    public static Sweet CreateChocolate()
+    {
+        return new Sweet("brown", "square", 25);
     }
 
 }
