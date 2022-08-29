ee4de8284d12240dcab6f6d078ff8a270ecc46d8
CurtisLV <curtislv@gmail.com>
2022-08-29 12:13:15 +0300

    Function creation

--- RadDegConverter/Program.cs
+++ RadDegConverter/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RadDegConverter;
+
+Console.WriteLine($"6.28 radians to degrees: {Converter.RadiansToDegrees(6.28)}");
+Console.WriteLine($"90 degrees to radians: {Converter.DegreesToRadians(90)}");
+Console.ReadKey();
