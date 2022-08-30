84d651d2d3e7105b2f32e06f6e1127e059e6689f
CurtisLV <curtislv@gmail.com>
2022-08-30 21:07:05 +0300

    30/08/2022 21:06:39

--- DateTimeTest/Program.cs
+++ DateTimeTest/Program.cs
@@ -7,3 +7,10 @@ Console.WriteLine(dateTime);
 
 DateTime dateTime1 = new DateTime(2016, 7, 5);
 Console.WriteLine(dateTime1);
+// output: 05/07/2016 00:00:00
+
+
+// Now
+DateTime dateTime2 = DateTime.Now;
+Console.WriteLine(dateTime2);
+// output: 30/08/2022 21:06:39
