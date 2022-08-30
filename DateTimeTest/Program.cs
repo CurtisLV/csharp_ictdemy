bbe1f1a912c37c972147d4f39ed49573578c9945
CurtisLV <curtislv@gmail.com>
2022-08-30 21:05:35 +0300

    Different types of datetime

--- DateTimeTest/Program.cs
+++ DateTimeTest/Program.cs
@@ -1,2 +1,9 @@
 ﻿
 DateTime dateTime = new DateTime();
+Console.WriteLine(dateTime);
+
+// output: 01 / 01 / 0001 00:00:00  
+
+
+DateTime dateTime1 = new DateTime(2016, 7, 5);
+Console.WriteLine(dateTime1);
