b35732158fc1e6daed01c67a414fe7d03d567336
CurtisLV <curtislv@gmail.com>
2022-08-30 21:09:26 +0300

    21:09

--- DateTimeTest/Program.cs
+++ DateTimeTest/Program.cs
@@ -10,7 +10,8 @@ Console.WriteLine(dateTime1);
 // output: 05/07/2016 00:00:00
 
 
-// Now
+// Now (Can use Today with time then 00:00:00)
 DateTime dateTime2 = DateTime.Now;
 Console.WriteLine(dateTime2);
 // output: 30/08/2022 21:06:39
+Console.WriteLine($"It is month number {dateTime2.Month} of the year {dateTime2.Year}.");
