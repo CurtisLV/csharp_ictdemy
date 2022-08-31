af8e504a86487bc9141ca30d1daf1623e3d089f4
CurtisLV <curtislv@gmail.com>
2022-08-31 20:04:44 +0300

    GetFriday13 start

---
+++ GetFriday13/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System;
+
+Console.WriteLine("Prints all Fridays 13th for entered interval:");
+Console.WriteLine("Enter the first date: ");
+DateTime date1 = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second date: ");
+DateTime date2 = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Fridays 13th: ");
+
