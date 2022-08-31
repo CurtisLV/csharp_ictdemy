932020f383e33998965b730156a401fd1b803b12
CurtisLV <curtislv@gmail.com>
2022-08-31 20:29:25 +0300

    Age calculation with leap year included

---
+++ BirthDateCalc/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your birth date: ");
+DateTime birthdate = DateTime.Parse(Console.ReadLine());
+DateTime today = DateTime.Today;
+
+int age = today.Year - birthdate.Year;
+if (birthdate.Date > today.AddYears(age))
+{
+    age--;
+}
+
+Console.WriteLine($"You're {age} years old and your birthday is coming up in {} days.");
