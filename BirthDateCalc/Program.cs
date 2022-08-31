625aa28029de603e02925fa61f396bbd30aa13f8
CurtisLV <curtislv@gmail.com>
2022-08-31 21:08:30 +0300

    Finalest

--- BirthDateCalc/Program.cs
+++ BirthDateCalc/Program.cs
@@ -5,4 +5,4 @@ string dateText = Console.ReadLine();
 DateTime birthday = DateTime.Parse(dateText);
 Person user = new Person(birthday);
 
-Console.WriteLine($"You're {user.GetAge()} years old and your birthday is coming up in {user.GetDaysToBirthday()} days.");
+Console.WriteLine($"You're {user.GetAge()} years old and your birthday is comming up in {user.GetDaysToBirthday()} days.");
