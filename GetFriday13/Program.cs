a04c0a77e87ecc1b97cfc38269cab0a85640097b
CurtisLV <curtislv@gmail.com>
2022-08-31 20:14:15 +0300

    20:14

--- GetFriday13/Program.cs
+++ GetFriday13/Program.cs
@@ -10,3 +10,20 @@ DateTime date2 = DateTime.Parse(Console.ReadLine());
 
 Console.Write("Fridays 13th: ");
 
+// try to find friday 
+while (date1.DayOfWeek != DayOfWeek.Friday)
+{
+    date1 = date1.AddDays(1);
+}
+
+// when friday, add 7 days, check if 13th print all dates while date2 is smaller
+
+while (date1 < date2)
+{
+    if (date1.Day == 13)
+    {
+        Console.Write(date1.ToShortDateString() + ", ");
+    }
+  
+    date1 = date1.AddDays(7);
+}
