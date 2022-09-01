fe73b79ada7b9e130719cd9db13fe661f01f9566
CurtisLV <curtislv@gmail.com>
2022-09-01 21:14:50 +0300

    Lottery class

---
+++ TheLottery/Lottery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLottery;
+
+internal class Lottery
+{
+    private List<int> numbers;
+}
