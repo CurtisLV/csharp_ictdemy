dcc94b2453e24c86b7e1d397bafa0822e126e9f9
CurtisLV <curtislv@gmail.com>
2022-09-01 21:23:25 +0300

    Adding methods Lot / Print

--- TheLottery/Lottery.cs
+++ TheLottery/Lottery.cs
@@ -16,4 +16,23 @@ internal class Lottery
         random = new Random();
         numbers = new List<int>();
     }
+
+
+    public int Lot()
+    {
+        int number = random.Next(100) + 1;
+        numbers.Add(number);
+        return number;
+    }
+
+    public string Print()
+    {
+        string s = "";
+        numbers.Sort();
+        foreach (int i in numbers)
+        {
+            s += i + " ";
+        }
+        return s;
+    }
 }
