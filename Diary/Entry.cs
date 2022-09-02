5b432c505444ca388f443fd9baf2982dc91c6c89
CurtisLV <curtislv@gmail.com>
2022-09-02 20:16:25 +0300

    Entry class logic

--- Diary/Entry.cs
+++ Diary/Entry.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Diary
+namespace Diary;
+
+internal class Entry
 {
-    internal class Entry
+    public DateTime Occurs
     {
+        get; set;
+    }
+
+    public string Text
+    {
+        get; set;
+    }
+
+    public Entry(DateTime occurs, string text)
+    {
+        Occurs = occurs;
+        Text = text;
+    }
+
+    public override string ToString()
+    {
+        return Occurs + " " + Text;
     }
 }
