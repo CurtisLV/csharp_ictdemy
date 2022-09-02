cc6cc603e4ba9cceadee123d35c8d1a727457230
CurtisLV <curtislv@gmail.com>
2022-09-02 20:45:56 +0300

    20:45

--- Diary/DiaryC.cs
+++ Diary/DiaryC.cs
@@ -8,4 +8,23 @@ namespace Diary;
 
 internal class DiaryC
 {
+    private Database database;
+
+    public DiaryC()
+    {
+        database = new Database();
+    }
+
+    public DateTime ReadDateTime()
+    {
+        Console.WriteLine("Enter date and time as e.g. [01/13/2016 14:00]:");
+        DateTime dateTime;
+
+        while (!DateTime.TryParse(Console.ReadLine(), out dateTime))
+        {
+            Console.WriteLine("Error. Please try again: ");
+        }
+        return dateTime;
+
+    }
 }
