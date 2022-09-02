754b0e0d35e1746ccd5560c1080b87c440f4c90f
CurtisLV <curtislv@gmail.com>
2022-09-02 21:14:12 +0300

    Main logic 1st

--- Diary/Program.cs
+++ Diary/Program.cs
@@ -1,1 +1,20 @@
 ﻿using Diary;
+
+DiaryC diary = new DiaryC();
+char choice = '0';
+
+// main loop 
+
+while (choice != '4') 
+{
+    diary.PrintHomeScreen();
+    Console.WriteLine();
+    Console.WriteLine("Choose an action:");
+    Console.WriteLine("1 - Add an entry");
+    Console.WriteLine("2 - Search for entries");
+    Console.WriteLine("3 - Delete entries");
+    Console.WriteLine("4 - End");
+    choice = Console.ReadKey().KeyChar;
+    Console.WriteLine();
+    // reaction to the choice
+}
