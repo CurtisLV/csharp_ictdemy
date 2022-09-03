624849d4ccfe8944b9ea1ea0dbd0e6a348fcf6a0
CurtisLV <curtislv@gmail.com>
2022-09-03 12:15:28 +0300

    Collecting words logic

---
+++ ListExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> words = new List<string>();
+
+Console.WriteLine("Enter a word: ");
+string answer = Console.ReadLine().ToLower();
+
+while (answer != "end") 
+{
+    Console.Write("Enter a word: ");
+    answer = Console.ReadLine().ToLower();
+    if (answer != "end")
+    {
+        words.Add(answer);
+    }
+}
+
+Console.WriteLine("You've entered the following words: ");
+foreach (string word in words) 
+{
+
+}
