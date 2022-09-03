8ce31f2f96f257c9a20b58ed0a9d55c04c309a1b
CurtisLV <curtislv@gmail.com>
2022-09-03 13:06:56 +0300

    Main logic part 1

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using CarDealership;
+
+Dealership carDealership = new Dealership("Skew street, 10", 500000);
+
+// Create testing data
+Car skoda120 = new Car("123ABC", "Škoda 120", 1000);
+Car skodaFabia = new Car("456DEF", "Škoda Fabia", 10000);
+Car ferrari = new Car("789GHI", "Ferrari FF", 300000);
+Truck tatra = new Truck("534NGS", "Tatra 811", 5000, 20000);
+Truck renault = new Truck("489GSR", "Renault Midliner", 10000, 30000);
+
+carDealership.AddCar(skoda120);
+carDealership.AddCar(skodaFabia);
+carDealership.AddCar(ferrari);
+carDealership.AddCar(tatra);
+carDealership.AddCar(renault);
+
+// Print statistics
