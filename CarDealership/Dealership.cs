c43cdb11c9c4ec5576ec59bf8742a0e544fe7997
CurtisLV <curtislv@gmail.com>
2022-09-03 12:46:07 +0300

    Dealership properties and constructor

--- CarDealership/Dealership.cs
+++ CarDealership/Dealership.cs
@@ -8,4 +8,28 @@ namespace CarDealership;
 
 internal class Dealership
 {
+    private string Address
+    {
+        get; set;
+    }
+    private int Money
+    {
+        get; set;
+    }
+
+    public int CarCount
+    {
+        get
+        {
+            return cars.Count;
+        }
+    }
+
+    private List<Car> cars = new List<Car>();
+
+    public Dealership(string address, int money)
+    {
+        Address = address;
+        Money = money;
+    }
 }
