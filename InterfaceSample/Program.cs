779b541a71f71a7070049efd887867386ebf2092
CurtisLV <curtislv@gmail.com>
2022-09-05 21:18:25 +0300

    Main logic

--- InterfaceSample/Program.cs
+++ InterfaceSample/Program.cs
@@ -9,3 +9,8 @@ IBird bird2 = new Pterodactyl();
 bird2.Breathe();
 
 ((Pterodactyl)bird2).Crawl();
+
+IBird bird3 = new Pterodactyl();
+bird.Breathe();
+ILizard lizard = new Pterodactyl();
+lizard.Breathe();
