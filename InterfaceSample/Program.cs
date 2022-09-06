954b1acc875cc50af9b058265f2a523e82350645
CurtisLV <curtislv@gmail.com>
2022-09-06 19:59:12 +0300

    Common ancestor and object hierarchy

--- InterfaceSample/Program.cs
+++ InterfaceSample/Program.cs
@@ -23,6 +23,8 @@ animals.Add(new Dolphin() { Weight = 9 });
 animals.Add(new Pterodactyl() { Weight = 3 });
 animals.Add(new Pterodactyl() { Weight = 2 });
 
+// type casting with Is keyword
+
 foreach (Animal animal in animals)
 {
     Console.WriteLine(animal);
@@ -32,4 +34,26 @@ foreach (Animal animal in animals)
         ((Dolphin)animal).Jump();
     }
 }
-    
+
+
+// type casting with As keyword
+// difference is that if the casting fails because the object doesn't provide the given interface, the result will be null
+
+foreach (Animal animal in animals)
+{
+    animal.Breathe();
+    if (animal is Dolphin)
+    {
+        (animal as Dolphin).Jump();
+    }
+}
+
+// typeof() - determine the object type
+
+foreach (Animal animal in animals)
+{
+    if (animal.GetType() == typeof(Dolphin))
+    {
+        (animal as Dolphin).Jump();
+    }
+}
