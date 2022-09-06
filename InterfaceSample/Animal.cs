2a1840e1fa04e9dcb77998d9f0dd78e6173e24ed
CurtisLV <curtislv@gmail.com>
2022-09-06 19:45:55 +0300

    Common ancestor and object hierarchy

---
+++ InterfaceSample/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSample;
+
+internal class Animal
+{
+    public int Weight {
+        get; set;
+    }
+
+    public void Breathe()
+    {
+        Console.WriteLine("Breathing...");
+    }
+}
