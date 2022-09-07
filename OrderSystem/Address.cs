a30a235dc6a0f9ed1c8770f591ff9698dc220d31
CurtisLV <curtislv@gmail.com>
2022-09-07 19:51:47 +0300

    Address class

--- OrderSystem/Address.cs
+++ OrderSystem/Address.cs
@@ -8,5 +8,23 @@ namespace OrderSystem
 {
     internal class Address
     {
+        public string Street { get; set; }
+
+        public int HouseNumber { get; set; }
+
+        public int RegistryNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public Address(string street, int houseNumber, int registryNumber, string city, string zipCode)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            RegistryNumber = registryNumber;
+            City = city;
+            ZipCode = zipCode;
+        }
     }
 }
--- OrderSystem/Program.cs
+++ OrderSystem/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using OrderSystem;
+
+Customer customer = new Customer(1, "Thomas", "Futile");
+Address address = new Address("Evergreen", 10, 2, "Prague", "10000");
