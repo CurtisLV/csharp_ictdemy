4ef532fd42a0b17398cc2344e1c2e330b1868c6a
CurtisLV <curtislv@gmail.com>
2022-09-07 19:47:50 +0300

    Customer class

--- OrderSystem/Customer.cs
+++ OrderSystem/Customer.cs
@@ -8,5 +8,17 @@ namespace OrderSystem
 {
     internal class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName {
+            get; set; 
+        }
+
+        public Customer(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
     }
 }
