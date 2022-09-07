be1e9c3224b7238ebd54674332e9fc1592e0c94f
CurtisLV <curtislv@gmail.com>
2022-09-07 18:58:46 +0300

    Operator overloading

---
+++ InterfaceSample/Lizard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSample;
+
+internal class Lizard: Animal, ILizard
+{
+    public void Crawl()
+    {
+        Console.WriteLine("Crawling...");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Crawling...");
+    }
+
+}
