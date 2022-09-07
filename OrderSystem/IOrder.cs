d4501afa52e6942f252a4ae6f17b8cc1b3b6d8da
CurtisLV <curtislv@gmail.com>
2022-09-07 19:44:42 +0300

    Create all classes

---
+++ OrderSystem/IOrder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem;
+
+internal interface IOrder
+{
+    int Number {
+        get;
+    }
+
+    string FirstName {
+        get;
+    }
+
+    string LastName {
+        get;
+    }
+
+    string Street {
+        get;
+    }
+
+    int HouseNumber {
+        get;
+    }
+
+    int RegistryNumber {
+        get;
+    }
+
+    string City {
+        get;
+    }
+
+    string Zip {
+        get;
+    }
+
+    string Country {
+        get;
+    }
+
+    string[] Products {
+        get;
+    }
+
+    int[] Quantities {
+        get;
+    }
+
+    double[] Prices {
+        get;
+    }
+}
---
+++ OrderSystem/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    internal class Order : IOrder
+    {
+        public int Number { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Street { get; set; }
+
+        public int HouseNumber { get; set; }
+
+        public int RegistryNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        public string[] Products { get; set; }
+
+        public int[] Quantities { get; set; }
+
+        public double[] Prices { get; set; }
+    }
+}
