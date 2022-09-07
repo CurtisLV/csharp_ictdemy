a483b894ae9637343174650eae254fa690cd0c39
CurtisLV <curtislv@gmail.com>
2022-09-07 20:08:28 +0300

    Order class 1st part

--- OrderSystem/Order.cs
+++ OrderSystem/Order.cs
@@ -8,21 +8,47 @@ namespace OrderSystem
 {
     internal class Order : IOrder
     {
-        public int Number { get; set; }
+        public int Number { get; private set; }
 
-        public string FirstName { get; set; }
+        public string FirstName {
+            get {
+                return customer.Name; 
+            }
+        }
 
-        public string LastName { get; set; }
+        public string LastName {
+            get {
+                return customer.LastName;
+            }
+        }
 
-        public string Street { get; set; }
+        public string Street {
+            get {
+                return addressInvoice.Street;
+            }
+        }
 
-        public int HouseNumber { get; set; }
+        public int HouseNumber {
+            get {
+                return addressInvoice.HouseNumber;
+            }
+        }
 
-        public int RegistryNumber { get; set; }
+        public int RegistryNumber {
+            get {
+                return addressInvoice.RegistryNumber;
+            }
+        }
 
-        public string City { get; set; }
+        public string City {
+            get { return addressInvoice.City; }
+        }
 
-        public string Zip { get; set; }
+        public string Zip {
+            get {
+                return addressInvoice.ZipCode;
+            }
+        }
 
         public string Country { get; set; }
 
@@ -31,5 +57,20 @@ namespace OrderSystem
         public int[] Quantities { get; set; }
 
         public double[] Prices { get; set; }
+
+        private Customer customer;
+        private Product product;
+        private Address addressShipping;
+        private Address addressInvoice;
+
+        public Order(int number, Product product, Customer customer, Address addressShipping, Address addressInvoice)
+        {
+            Number = number;
+            this.product = product;
+            this.customer = customer;
+            this.addressInvoice = addressInvoice;
+            this.addressShipping = addressShipping;
+
+        }
     }
 }
