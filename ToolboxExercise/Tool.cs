7a49079bdb52e59935966a899811f45e8a6d5bd3
CurtisLV <curtislv@gmail.com>
2022-09-08 21:31:52 +0300

    Common ancestor Tool

--- ToolboxExercise/Tool.cs
+++ ToolboxExercise/Tool.cs
@@ -8,4 +8,21 @@ namespace ToolboxExercise;
 
 internal abstract class Tool
 {
+
+    public int Weight { get; private set; }
+    public string Name { get; private set; }
+
+    public Tool(int weight, string name)
+    {
+        Weight = weight;
+        Name = name;
+    }   
+
+    public abstract void Work();
+
+    public override string ToString()
+    {
+        return Name;
+    }
+
 }
