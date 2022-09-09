baf15b089d9d90d7d3729c81cda20ff590b9161c
Mukāns, Kārlis <karlis.mukans@seb.se>
2022-09-09 16:33:23 +0300

    Electric Screwdriver class

---
+++ ToolboxExercise/ElectricalScrewdriver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolboxExercise;
+
+internal class ElectricalScrewdriver : Screwdriver
+{
+    public int BatteryLevel { get; private set; }
+    public ElectricalScrewdriver(int weight, string name, int batteryLevel) : base(weight, name)
+    {
+        BatteryLevel = batteryLevel;
+    }
+
+    public override void Work()
+    {
+        base.Work();
+    }
+
+}
