66c76b66adb4f2c8b742850e7f868b2b0c52a1cf
Mukāns, Kārlis <karlis.mukans@seb.se>
2022-09-09 16:40:05 +0300

    Toolbox constructor, methods

--- ToolboxExercise/Toolbox.cs
+++ ToolboxExercise/Toolbox.cs
@@ -8,4 +8,30 @@ namespace ToolboxExercise;
 
 internal class Toolbox
 {
+    public int MaxWeight { get; private set; }
+
+    public int MaxTools { get; private set; }
+
+    public int CurrentWeight { get; private set; }
+
+    private List<Tool> tools = new List<Tool>();
+
+    public Toolbox(int maxWeight, int maxTools, int currentWeight, List<Tool> tools)
+    {
+        MaxWeight = maxWeight;
+        MaxTools = maxTools;
+        CurrentWeight = currentWeight;
+        this.tools = tools;
+    }
+
+    public void AddTool(Tool tool) 
+    {
+        tools.Add(tool);
+    }
+
+    public void RemoveTool(Tool tool) 
+    {
+        tools.Remove(tool);
+    }
+
 }
