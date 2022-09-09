e3b0b53c3a375ea91947a2959152bbd711d61b37
Mukāns, Kārlis <karlis.mukans@seb.se>
2022-09-09 16:28:42 +0300

    Screwdriver class

--- ToolboxExercise/Screwdriver.cs
+++ ToolboxExercise/Screwdriver.cs
@@ -8,4 +8,13 @@ namespace ToolboxExercise;
 
 internal class Screwdriver: Tool
 {
+
+    public Screwdriver(int weight, string name) : base(weight, name) 
+    { 
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("I'm screwing");
+    }
 }
