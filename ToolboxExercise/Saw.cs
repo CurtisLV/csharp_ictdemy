f9d7812d80546883fb2ed8e9016d986558c80524
Mukāns, Kārlis <karlis.mukans@seb.se>
2022-09-09 16:30:26 +0300

    Saw class

--- ToolboxExercise/Saw.cs
+++ ToolboxExercise/Saw.cs
@@ -8,4 +8,12 @@ namespace ToolboxExercise;
 
 internal class Saw: Tool
 {
+    public Saw(int weight, string name) : base(weight, name)
+    {
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("I'm sawing");
+    }
 }
