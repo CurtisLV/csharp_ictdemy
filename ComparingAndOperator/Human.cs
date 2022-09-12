1fb2106117e7272376afa02c6ff253ad056f1d84
Mukāns, Kārlis <karlis.mukans@seb.se>
2022-09-12 15:15:57 +0300

    Abstract Human class

---
+++ ComparingAndOperator/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingAndOperator
+{
+    internal abstract class Human
+    {
+
+        public string FirstName;
+        public string LastName;
+        public string BirthDate;
+
+
+    }
+}
