daf2a772b14b1004a150a6529e5ca593fcf38b84
CurtisLV <curtislv@gmail.com>
2022-09-14 21:26:13 +0300

    Minor changes

--- ComparingAndOperator/Human.cs
+++ ComparingAndOperator/Human.cs
@@ -4,56 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComparingAndOperator
+namespace ComparingAndOperator;
+
+internal abstract class Human : IComparable 
 {
-    internal abstract class Human : IComparable 
+
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    private static Random random = new Random();
+    public Human(string name, string lastName, DateTime birthDate)
     {
+        Name = name;
+        LastName = lastName;
+        BirthDate = birthDate;
+    }
 
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
+    public int CompareTo(object obj)
+    {
+        Human other = obj as Human;
 
-        private static Random random = new Random();
-        public Human(string name, string lastName, DateTime birthDate)
+        if (Name == other.Name && LastName == other.LastName)
         {
-            Name = name;
-            LastName = lastName;
-            BirthDate = birthDate;
+            return BirthDate.CompareTo(other.BirthDate);
+        }
+        return Name.CompareTo(other.Name);
+    }
+
+    // overload + operator
+    public static Human operator +(Human human1, Human human2)
+    {
+        if (human1.GetType() == human2.GetType())
+        {
+            return null;
         }
 
-        public int CompareTo(object obj)
+        Man man = (human1 is Man) ? (Man)human1 : (Man)human2;
+        Woman woman = (human1 is Woman) ? (Woman)human1 : (Woman)human2;
+
+        if (random.Next(2) == 0)
         {
-            Human other = obj as Human;
-
-            if (Name == other.Name && LastName == other.LastName)
-            {
-                return BirthDate.CompareTo(other.BirthDate);
-            }
-            return Name.CompareTo(other.Name);
+            return new Man("Adam", man.LastName, DateTime.Now);
         }
+        return new Woman("Eve", woman.LastName, DateTime.Now);
+    }
 
-        // overload + operator
-        public static Human operator +(Human human1, Human human2)
-        {
-            if (human1.GetType() == human2.GetType())
-            {
-                return null;
-            }
-
-            Man man = (human1 is Man) ? (Man)human1 : (Man)human2;
-            Woman woman = (human1 is Woman) ? (Woman)human1 : (Woman)human2;
-
-            if (random.Next(2) == 0)
-            {
-                return new Man("Adam", man.LastName, DateTime.Now);
-            }
-            return new Woman("Eve", woman.LastName, DateTime.Now);
-        }
-
-        // override ToString()
-        public override string ToString()
-        {
-            return $"{Name} {LastName} {BirthDate.ToString("yy")}";
-        }
+    // override ToString()
+    public override string ToString()
+    {
+        return $"{Name} {LastName} {BirthDate.ToString("yy")}";
     }
 }
