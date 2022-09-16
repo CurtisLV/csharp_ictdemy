4f33df62f8c46b6ee9ce97113a5a95c069939d72
CurtisLV <curtislv@gmail.com>
2022-09-16 20:59:57 +0300

    Using delegate in Main

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,3 +4,12 @@ static int Square(int a)
 {
     return a * a;
 }
+
+Number numbers = new Number();
+
+Console.WriteLine(numbers);
+
+
+numbers.PerformOperation(Square);
+
+Console.WriteLine(numbers);
