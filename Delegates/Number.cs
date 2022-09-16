80eba5681f17de0519f25c61de6727a77a6e5147
CurtisLV <curtislv@gmail.com>
2022-09-16 20:43:40 +0300

    Number class

--- Delegates/Number.cs
+++ Delegates/Number.cs
@@ -9,4 +9,23 @@ namespace Delegates;
 internal class Number
 {
     private List<int> numbers = new List<int>();
+
+    public Number()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            numbers.Add(i + 1);
+        }
+    }
+
+    public override string ToString()
+    {
+        string output = "";
+        foreach (int number in numbers)
+        {
+            output += number + ", ";
+        }
+        return output;
+    }
 }
+
