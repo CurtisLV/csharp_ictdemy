bc169496fd17c292b84e25ef958276c31e1e39be
CurtisLV <curtislv@gmail.com>
2022-09-16 21:11:42 +0300

    Anonymous delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,21 +1,34 @@
 ﻿using Delegates;
 
-static int Square(int a)
-{
-    return a * a;
-}
+//static int Square(int a)
+//{
+//    return a * a;
+//}
 
 Number numbers = new Number();
 Console.WriteLine(numbers);
 
-numbers.PerformOperation(Square);
+////numbers.PerformOperation(Square);
+////Console.WriteLine(numbers);
+
+//static int Double(int a)
+//{
+//    return a * 2;
+//}
+
+//numbers.PerformOperation(Double);
+
+//Console.WriteLine(numbers);
+
+numbers.PerformOperation(delegate (int a)
+{
+    return a * a;
+});
+
 Console.WriteLine(numbers);
 
-static int Double(int a)
+numbers.PerformOperation(delegate (int a)
 {
     return a * 2;
-}
-
-numbers.PerformOperation(Double);
-
+});
 Console.WriteLine(numbers);
