d6e7a44ed16c6d46472aa483b0bcc50fdb3e3e2b
CurtisLV <curtislv@gmail.com>
2022-09-16 20:40:50 +0300

    Delegates class

---
+++ Delegates/Number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates;
+
+internal class Number
+{
+    private List<int> numbers = new List<int>();
+}
