7b49d0c2f0cdabab6e5d3c7c36ce89848e36b58c
CurtisLV <curtislv@gmail.com>
2022-09-17 20:04:34 +0300

    User class

---
+++ OnlineDating/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineDating;
+
+internal class User
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime Birthdate { get; private set; }
+
+    public enum Sex
+    {
+        Male, Female
+    };
+
+
+}
