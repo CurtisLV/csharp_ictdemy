8ee44ff13390d6645ff1a7926cd686bb94986962
CurtisLV <curtislv@gmail.com>
2022-09-17 20:37:16 +0300

    Moving enums out of class and properly referencing in class

--- OnlineDating/User.cs
+++ OnlineDating/User.cs
@@ -5,38 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineDating;
-
+public enum Sex
+{
+    Male, Female
+}
+public enum HairColor
+{
+    NotSpecified, Blonde, Brown, Black, Redhead
+}
+public enum EyeColor
+{
+    NotSpecified = 0,
+    Blue = 1,
+    Green = 2,
+    Brown = 4,
+    Black = 8,
+    Cyan = 3,
+    Browngreen = 6
+}
 internal class User
 {
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public DateTime Birthdate { get; private set; }
+    public Sex Sex { get; set; }
+    public EyeColor EyeColor { get; set; }
 
-    public enum Sex
-    {
-        Male, Female
-    }
-    public enum HairColor
-    {
-        NotSpecified, Blonde, Brown, Black, Redhead
-    }
-    
-    [Flags]
-    public enum EyeColor
-    {
-        NotSpecified = 0, 
-        Blue = 1, 
-        Green = 2, 
-        Brown = 4, 
-        Black = 8, 
-        Cyan = 3, 
-        Browngreen = 6
-    }
+    public HairColor HairColor { get; set; }    
 
-    public User(string firstName, string lastName, DateTime birthdate)
+    public User(string firstName, string lastName, DateTime birthdate, Sex sex, EyeColor eyeColor, HairColor hairColor)
     {
         FirstName = firstName;
         LastName = lastName;
         Birthdate = birthdate;
+        EyeColor = eyeColor;
+        HairColor = hairColor;
+        Sex = sex;
     }
+
+
+
+
 }
