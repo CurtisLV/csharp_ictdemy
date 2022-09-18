273f3d83ea77e171b08ca9fb5e203a154ddc5c23
CurtisLV <curtislv@gmail.com>
2022-09-18 19:03:27 +0300

    Add method

--- OwnList/MyList.cs
+++ OwnList/MyList.cs
@@ -8,4 +8,20 @@ namespace OwnList;
 
 internal class MyList
 {
+
+    private object[] items = new object[10];
+
+    public int Count { get; private set; }
+
+    public void Add(object item)
+    {
+        items[Count] = item;
+        Count++;
+
+        if (Count == 10)
+        {
+
+        }
+    }
+
 }
