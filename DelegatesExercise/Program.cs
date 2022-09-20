aefd21e3b1896d06097aaa128b20cd91c36fadd2
CurtisLV <curtislv@gmail.com>
2022-09-20 19:27:08 +0300

    Aggregate delegate

--- DelegatesExercise/Program.cs
+++ DelegatesExercise/Program.cs
@@ -3,3 +3,7 @@
 Sentence sentence = new Sentence("Delegates in C# .NET work as method references.");
 sentence.Map(s => s.ToUpper());
 Console.WriteLine(sentence);
+
+
+Sentence sentence2 = new Sentence("Delegates in C# .NET work like method references.");
+Console.WriteLine(sentence.Aggregate((a, b) => a + "-" + b));
