02eef20cea221688f9ed288baef17ae3d5f26a8b
CurtisLV <curtislv@gmail.com>
2022-09-20 19:47:14 +0300

    Building main logic 3

--- DelegatesExercise/Program.cs
+++ DelegatesExercise/Program.cs
@@ -4,13 +4,8 @@ Sentence sentence = new Sentence("Delegate in C# .NET work like method reference
 sentence.Map(s => (s.Length > 5) ? s.ToUpper() : s);
 Console.WriteLine(sentence);
 
-
 string aggregate1 = sentence.Aggregate((a,b) => a + " " + b);
-
-Console.WriteLine(aggregate1);
-
 string aggregate2 = sentence.Aggregate((a, b) => "(" + a + ") " + b);
-Console.WriteLine(aggregate2);
 string aggregate3 = sentence.Aggregate((a,b) => a + " " + ((b.Length > 4) ? b.Substring(0, 3) + "." : b));
-Console.WriteLine(aggregate3);
 
+Console.WriteLine($"Aggregates: \n{aggregate1}\n{aggregate2}\n{aggregate3}");
