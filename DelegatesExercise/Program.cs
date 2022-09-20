b4905144fce7607c83f4a5b70476773777703ab1
CurtisLV <curtislv@gmail.com>
2022-09-20 19:45:57 +0300

    Building main logic 2

--- DelegatesExercise/Program.cs
+++ DelegatesExercise/Program.cs
@@ -1,6 +1,6 @@
 ﻿using DelegatesExercise;
 
-Sentence sentence = new Sentence("Delegates in C# .NET work as method references.");
+Sentence sentence = new Sentence("Delegate in C# .NET work like method reference.");
 sentence.Map(s => (s.Length > 5) ? s.ToUpper() : s);
 Console.WriteLine(sentence);
 
@@ -8,14 +8,9 @@ Console.WriteLine(sentence);
 string aggregate1 = sentence.Aggregate((a,b) => a + " " + b);
 
 Console.WriteLine(aggregate1);
-//string aggregate2 = 
-//string aggregate3 = 
 
-
-//Sentence sentence2 = new Sentence("Delegates in C# .NET work like method references.");
-//Console.WriteLine(sentence2.Aggregate((a, b) => a + "-" + b));
-
-//Sentence sentence3 = new Sentence("Delegates in C# .NET work like method references.");
-
-//Console.WriteLine(sentence3);
+string aggregate2 = sentence.Aggregate((a, b) => "(" + a + ") " + b);
+Console.WriteLine(aggregate2);
+string aggregate3 = sentence.Aggregate((a,b) => a + " " + ((b.Length > 4) ? b.Substring(0, 3) + "." : b));
+Console.WriteLine(aggregate3);
 
