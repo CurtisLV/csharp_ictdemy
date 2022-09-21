236f4390dc91dc02430b8bb8824b5aa5f87e9769
CurtisLV <curtislv@gmail.com>
2022-09-21 22:30:31 +0300

    Main logic update

--- DelegatesExercise/Program.cs
+++ DelegatesExercise/Program.cs
@@ -10,5 +10,5 @@ string aggregate3 = sentence.Aggregate((a,b) => a + " " + ((b.Length > 4) ? b.Su
 
 Console.WriteLine($"Aggregates: \n{aggregate1}\n{aggregate2}\n{aggregate3}");
 
-string aggregate4 = sentence.Aggregate((a, b) => a + "yOlO " + ((b.Length > 7) ? b.ToLower() : b.ToUpper()));
+string aggregate4 = sentence.Aggregate((a, b) => a + " yOlO " + ((b.Length > 5) ? b.ToLower() : b.ToUpper()));
 Console.WriteLine(aggregate4);
