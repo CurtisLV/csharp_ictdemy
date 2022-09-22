c200bc0c9c6b4de876044edc6d20e79a5417e3c6
CurtisLV <curtislv@gmail.com>
2022-09-22 15:14:00 +0300

    Customer class

--- Events/Customer.cs
+++ Events/Customer.cs
@@ -8,4 +8,16 @@ namespace Events;
 
 internal class Customer
 {
+    private List<Order> orders = new List<Order>();
+
+    public void AddOrder(Order order)
+    {
+        orders.Add(order);
+        order.StateChanged += Notify;
+    }
+
+    public void Notify(object sender, EventArgs e)
+    {
+        Console.WriteLine($"Order state of {sender} changed to {(sender as Order).State}");
+    }
 }
