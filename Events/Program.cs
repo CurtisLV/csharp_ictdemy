5a64987e0ae184afcff01c1f6684108f0f66a266
CurtisLV <curtislv@gmail.com>
2022-09-22 15:17:10 +0300

    Main logic 1

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,1 +1,12 @@
 ﻿using Events;
+
+Customer customer = new Customer();
+Order objNotebook = new Order("Notebook");
+Order objCellPhone = new Order("Cell phone");
+
+customer.AddOrder(objNotebook);
+customer.AddOrder(objCellPhone);
+
+objCellPhone.ChangeState(Order.EState.Confirmed);
+objNotebook.ChangeState(Order.EState.Confirmed);
+objCellPhone.ChangeState(Order.EState.Shipped);
