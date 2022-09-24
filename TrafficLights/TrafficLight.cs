a0f5f7db18d53c7711d81e8f6c2c850905511f69
CurtisLV <curtislv@gmail.com>
2022-09-24 18:04:29 +0300

    Traffic light class

--- TrafficLights/TrafficLight.cs
+++ TrafficLights/TrafficLight.cs
@@ -6,6 +6,31 @@ using System.Threading.Tasks;
 
 namespace TrafficLights;
 
+public enum ELights
+{
+    Green, Yellow, Red
+}
+
 internal class TrafficLight
 {
+    public event EventHandler OnLightChange; 
+    private ELights lights;
+    public ELights Lights {
+        get {
+            return lights;
+        } 
+        set {
+            lights = value;
+            OnLightChangeEvent();
+        } 
+    }
+
+    public void OnLightChangeEvent()
+    {
+        if (OnLightChange != null)
+        {
+            OnLightChange(this, EventArgs.Empty);
+        }
+    }
 }
+
