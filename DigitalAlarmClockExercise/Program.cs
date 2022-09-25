1e050f3908b29ba6c5bae0686561f60911e06a23
CurtisLV <curtislv@gmail.com>
2022-09-25 18:00:08 +0300

    Main logic

--- DigitalAlarmClockExercise/Program.cs
+++ DigitalAlarmClockExercise/Program.cs
@@ -7,4 +7,11 @@ Grandma granny = new Grandma();
 AlarmClock alarm = new AlarmClock();
 DigitalAlarmClock digiAlarm = new DigitalAlarmClock();
 
+alarm.Ringing += human.WakeUp;
+alarm.Ringing += granny.WakeUp;
 
+digiAlarm.Ringing += human.WakeUp;
+digiAlarm.Ringing += granny.WakeUp;
+
+alarm.Ring();
+digiAlarm.Ring();
