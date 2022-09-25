decfef49b82b45eaa4e19c7350388f088c20e1a8
CurtisLV <curtislv@gmail.com>
2022-09-25 16:05:59 +0300

    Events 3 - still learning events

--- Events3/Button.cs
+++ Events3/Button.cs
@@ -8,4 +8,9 @@ namespace Events3;
 
 internal class Button
 {
+    public event EventHandler ButtonClicked;
+    protected virtual void OnClick()
+    {
+        ButtonClicked.Invoke(this, EventArgs.Empty);
+    }
 }
--- Events3/Program.cs
+++ Events3/Program.cs
@@ -1,1 +1,1 @@
-﻿
+﻿using Events3;
