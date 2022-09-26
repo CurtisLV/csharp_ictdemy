ed3e728756467935fafb1cdbade1c9020a7fb2fe
CurtisLV <curtislv@gmail.com>
2022-09-26 21:48:02 +0300

    main logic 1

--- UserProcessingSystemEVENTS/Program.cs
+++ UserProcessingSystemEVENTS/Program.cs
@@ -1,1 +1,20 @@
 ﻿using UserProcessingSystemEVENTS;
+
+
+Database db = new Database();
+EmailGen eg = new EmailGen();
+IdCardGenerator id = new IdCardGenerator();
+
+
+UserProcessor.UserProcesorEvent += db.SaveToDB;
+UserProcessor.UserProcesorEvent += eg.SendEmail;
+UserProcessor.UserProcesorEvent += id.GenerateIdCard;
+
+Console.WriteLine("Commands accepted!");
+Console.WriteLine("1. new : Add new user");
+Console.WriteLine("2. exit : Close application ");
+
+while (true)
+{
+
+}
