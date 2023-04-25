abfb3580c2ced4c47e4aa3883bcc2b638e9fa33f
CurtisLV <curtislv@gmail.com>
2023-04-25 17:06:52 +0300

    25.04 6

--- Arena/Mage.cs
+++ Arena/Mage.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 namespace Arena;
 
-internal class Mage: Warrior
+internal class Mage : Warrior
 {
     private int mana;
     private int maxMana;
     private int magicDamage;
 
-    public Mage(string name, int health, int damage, int defense, RollingDie die, int mana, int magicDamage): base(name, health, damage, defense, die)
+    public Mage(
+        string name,
+        int health,
+        int damage,
+        int defense,
+        RollingDie die,
+        int mana,
+        int magicDamage
+    )
+        : base(name, health, damage, defense, die)
     {
         this.mana = mana;
         this.maxMana = mana;
         this.magicDamage = magicDamage;
     }
-
 
     public override void Attack(Warrior enemy)
     {
@@ -32,22 +40,19 @@ internal class Mage: Warrior
             {
                 mana = maxMana;
             }
-            base.Attack(enemy); // if we are not using mana, we just use Base's Attack method 
-        } else // Magic damage
+            base.Attack(enemy); // if we are not using mana, we just use Base's Attack method
+        }
+        else // Magic damage
         {
             hit = magicDamage + die.Roll();
             SetMessage($"{name} attacks with a hit worth {hit} HP");
             enemy.Defend(hit);
             mana = 0;
         }
-        
-
     }
 
     public string ManaBar()
     {
         return GraphicalBar(mana, maxMana);
     }
-
-
 }
--- Arena/Program.cs
+++ Arena/Program.cs
@@ -4,7 +4,6 @@ RollingDie die = new RollingDie(10);
 
 Warrior karlis = new Warrior("Karlis", 100, 20, 10, die);
 
-
 //Warrior shadow = new Warrior("Shadow", 60, 18, 15, die);
 Warrior gandalf = new Mage("Gandalf", 60, 15, 12, die, 30, 45);
 
