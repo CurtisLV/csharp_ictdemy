cfae07502bbc3d40279c6e5828b6a02b8b05508d
CurtisLV <curtislv@gmail.com>
2023-04-25 17:09:00 +0300

    25.04. 7

--- OrderSystem/Program.cs
+++ OrderSystem/Program.cs
@@ -3,8 +3,11 @@
 Customer customer = new Customer(1, "Thomas", "Futile");
 Address address = new Address("Evergreen", 10, 2, "Prague", "10000");
 
-Product product = new Product("ict.social points", "Points to access premium content and achieve your dream job in IT.", 18);
-
+Product product = new Product(
+    "ict.social points",
+    "Points to access premium content and achieve your dream job in IT.",
+    18
+);
 
 Order order = new Order(1, product, customer, address, address);
 
