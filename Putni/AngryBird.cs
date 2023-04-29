86d19cbb0896645643b8dac6baf860f314781583
CurtisLV <curtislv@gmail.com>
2023-04-29 18:38:41 +0300

    29.04. 2

--- Putni/AngryBird.cs
+++ Putni/AngryBird.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Putni;
 
-namespace Putni;
-
-internal class AngryBird: Bird
+internal class AngryBird : Bird
 {
     private int anger = 50;
-
 
     public void Provoke(int infuriation)
     {
@@ -27,6 +20,4 @@ internal class AngryBird: Bird
     {
         return $"I'm an angry-bird with a weight of {weight}, a hunger level of {hunger}, and an anger level of {anger}.";
     }
-
-
 }
