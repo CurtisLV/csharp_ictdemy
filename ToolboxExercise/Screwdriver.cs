d806fc12500ed22e9b9a13e5e3e9e5908d3bbd6e
CurtisLV <curtislv@gmail.com>
2023-04-30 12:04:19 +0300

    30.04. 4

--- ToolboxExercise/Screwdriver.cs
+++ ToolboxExercise/Screwdriver.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace ToolboxExercise;
 
-internal class Screwdriver: Tool
+internal class Screwdriver : Tool
 {
-
-    public Screwdriver(int weight, string name) : base(weight, name) 
-    { 
-    }
+    public Screwdriver(int weight, string name)
+        : base(weight, name) { }
 
     public override void Work()
     {
