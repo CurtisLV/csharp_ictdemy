10f678633e9b4184bb63a36719ba37c7a71d8827
CurtisLV <curtislv@gmail.com>
2023-05-02 18:31:02 +0300

    02.05. 1

--- Calculator/CalculatorLogic.cs
+++ Calculator/CalculatorLogic.cs
@@ -10,6 +10,7 @@ namespace Calculator
     {
         double num1 = 0;
         double num2 = 0;
+
         public CalculatorLogic(double num1, double num2)
         {
             this.num1 = num1;
@@ -28,12 +29,12 @@ namespace Calculator
 
         public double Product()
         {
-            return (num1 * num2);  
+            return (num1 * num2);
         }
 
         public double Quotien()
         {
-            return num1 / num2; 
+            return num1 / num2;
         }
     }
 }
