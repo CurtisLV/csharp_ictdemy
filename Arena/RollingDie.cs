10dc0360239a237a0aa05fa09f9382706cc3a022
CurtisLV <curtislv@gmail.com>
2023-05-05 21:40:32 +0300

    Cleanup 4

--- Arena/RollingDie.cs
+++ Arena/RollingDie.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Arena;
+﻿namespace Arena;
 
 /// <summary>
 /// Class representing a die for a board game
