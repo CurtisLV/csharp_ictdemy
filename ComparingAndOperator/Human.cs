e13230d76b4bbc4d34f4b817ad0a402a71d87e3f
CurtisLV <curtislv@gmail.com>
2023-05-06 21:48:37 +0300

    Cleanup 1

--- ComparingAndOperator/Human.cs
+++ ComparingAndOperator/Human.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ComparingAndOperator;
+﻿namespace ComparingAndOperator;
 
 internal abstract class Human : IComparable
 {
