777ae4ccc81dc2d08d3784d62d264c5cec7b4bd1
CurtisLV <curtislv@gmail.com>
2023-05-07 14:03:34 +0300

    Cleanup 13

--- Events3/Button.cs
+++ Events3/Button.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Events3;
+﻿namespace Events3;
 
 internal class Button
 {
