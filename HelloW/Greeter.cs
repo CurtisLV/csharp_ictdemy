12a6f8ecf09eac7313c72962bd997204f62989d2
CurtisLV <curtislv@gmail.com>
2023-05-07 14:05:19 +0300

    Cleanup 17

--- HelloW/Greeter.cs
+++ HelloW/Greeter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloW
+﻿namespace HelloW
 {
     /// <summary>
     /// A class represents a greeter whose purpose is to greet the user
